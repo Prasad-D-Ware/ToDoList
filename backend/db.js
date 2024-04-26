edb96b956421004e8f5f519b5d300113ea8591ea
Prasad-D-Ware <prasadware42116@gmail.com>
2024-04-26 21:02:55 +0530

    initialised express routes and db schema

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { string, boolean } = require("zod");
+
+mongoose.connect(
+  "mongodb+srv://prasadware:QTK1BMd6m4el3UH2@cluster0.0ikj5rw.mongodb.net/to-do-app"
+);
+
+const todoSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  completed: Boolean,
+});
+const todos = mongoose.model("todos", todoSchema);
+
+module.exports = {
+  todo,
+};
